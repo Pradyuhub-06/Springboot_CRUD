e324b7a019a65933851b284a445a6f4d49ba3fca
Pradyuhub-06 <naga.kanna06@gmail.com>
2025-10-05 12:24:45 +0530

    Initial project setup commit

---
+++ SpringBootRestAPIProject/src/main/java/com/restapicrud/SpringBootRestAPIProject/SpringBootRestApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.restapicrud.SpringBootRestAPIProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiProjectApplication.class, args);
+	}
+
+}
